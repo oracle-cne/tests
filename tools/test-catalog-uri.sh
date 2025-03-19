652598b70546659c674b99a3f6b2bc8c3250206b
Daniel Krasinski <daniel.krasinski@oracle.com>
2025-03-19 19:17:35 +0000

    Copyrights

--- tools/test-catalog-uri.sh
+++ tools/test-catalog-uri.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
+#
+# Copyright (c) 2025, Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 export IFACE=$(ip route | grep default | head -1 | grep -o 'dev [a-z0-9]*' | cut -d' ' -f2)
 export IP_ADDR=$(ip addr show dev $IFACE | grep -o 'inet [0-9.]*' | cut -d' ' -f 2)
