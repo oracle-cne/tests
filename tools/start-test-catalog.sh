2e7c2b43049cb39d81d720c706c46d88248a7d8f
Daniel Krasinski <daniel.krasinski@oracle.com>
2025-04-29 20:06:01 +0000

    Add various configuration options

--- tools/start-test-catalog.sh
+++ tools/start-test-catalog.sh
@@ -3,8 +3,16 @@
 # Copyright (c) 2025, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
+USE_PODMAN="$2"
+
 pushd fixtures/catalog
-sh generate.sh "$1"
+sh generate.sh "$1" "$USE_PODMAN"
 popd # fixtures/catalog
 
-podman run -d --name ocnetestcatalog --rm -p 8080:80 ocne/testcatalog:latest
+if [ "$USE_PODMAN" = "true" ]; then
+	podman run -d --name ocnetestcatalog --rm -p 8080:80 ocne/testcatalog:latest
+else
+	pushd fixtures/catalog
+	nginx -p `pwd` -c ./nginx-local.conf -e /dev/null
+	popd # fixtures/catalog
+fi
--- tools/stop-test-catalog.sh
+++ tools/stop-test-catalog.sh
@@ -3,4 +3,12 @@
 # Copyright (c) 2025, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
-podman stop ocnetestcatalog
+USE_PODMAN="$1"
+
+if [ "$USE_PODMAN" = "true" ]; then
+	podman stop ocnetestcatalog
+else
+	pushd fixtures/catalog
+	kill $(cat nginx.pid)
+	popd # fixtures/catalog
+fi
