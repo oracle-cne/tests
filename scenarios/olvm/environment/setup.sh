dae36f6d65f4b3b286f214055af03e89fdc105a5
Michael Gianatassio <michael.gianatassio@oracle.com>
2025-05-23 07:45:41 -0400

    Add OLVM test scenario
    save
    
    save
    
    save
    
    save
    
    save
    
    save
    
    save
    
    save
    
    save
    
    save
    
    update olvm to use 3 worker nodes
    
    update olvm to use 3 worker nodes
    
    default vnic name
    
    rename OLVM_GATEWAY_IP to OLVM_NETWORK_GATEWAY_IP
    
    save
    
    save

---
+++ scenarios/olvm/environment/setup.sh
@@ -0,0 +1,109 @@
+#! /bin/bash
+#
+# Copyright (c) 2025, Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+TESTDIR=./scenarios/olvm
+
+
+# The OLVM environment variables are either set manually by the local user
+# or by the environment of a build job running the tests.
+if [ -z "$OLVM_SERVER_URL" ]; then
+	echo OLVM_SERVER_URL is not defined
+	exit 1
+fi
+
+if [ -z "$OLVM_VIRTUAL_IP" ]; then
+	echo OLVM_VIRTUAL_IP is not defined
+	exit 1
+fi
+
+if [ -z "$OLVM_SUBNET_BASE" ]; then
+	echo OLVM_SUBNET_BASE is not defined
+	exit 1
+fi
+
+if [ -z "$OLVM_STARTING_IPV4_ADDRESS_CP" ]; then
+	echo OLVM_STARTING_IPV4_ADDRESS_CP is not defined
+	exit 1
+fi
+
+if [ -z "$OLVM_ENDING_IPV4_ADDRESS_CP" ]; then
+	echo OLVM_ENDING_IPV4_ADDRESS_CP is not defined
+	exit 1
+fi
+
+if [ -z "$OLVM_STARTING_IPV4_ADDRESS_W" ]; then
+	echo OLVM_STARTING_IPV4_ADDRESS_W is not defined
+	exit 1
+fi
+
+if [ -z "$OLVM_ENDING_IPV4_ADDRESS_W" ]; then
+	echo OLVM_ENDING_IPV4_ADDRESS_W is not defined
+	exit 1
+fi
+
+if [ -z "$OLVM_DATACENTER_NAME" ]; then
+  export OLVM_DATACENTER_NAME=default
+	echo Defaulting OLVM_DATACENTER_NAME to "default"
+fi
+
+if [ -z "$OLVM_STORAGE_DOMAIN_NAME" ]; then
+	echo OLVM_STORAGE_DOMAIN_NAME is not defined
+	exit 1
+fi
+
+if [ -z "$OLVM_NETWORK_NAME" ]; then
+  export OLVM_NETWORK_NAME=vlan
+	echo Defaulting OLVM_NETWORK_NAME to "vlan"
+fi
+
+if [ -z "$OLVM_NETWORK_GATEWAY_IP" ]; then
+	echo OLVM_NETWORK_GATEWAY_IP is not defined
+	exit 1
+fi
+
+if [ -z "$OLVM_NETWORK_VNIC_NAME" ]; then
+  export OLVM_NETWORK_VNIC_NAME=nic-1
+	echo Defaulting OLVM_NETWORK_VNIC_NAME to "nic-1"
+fi
+
+if [ -z "$OLVM_NETWORK_INTERFACE" ]; then
+  export OLVM_NETWORK_INTERFACE=enp1s0
+	echo Defaulting OLVM_NETWORK_INTERFACE to "enp1s0"
+fi
+
+if [ -z "$OLVM_NETWORK_INTERFACE_TYPE" ]; then
+  export OLVM_NETWORK_INTERFACE_TYPE=virtio
+	echo Defaulting OLVM_NETWORK_INTERFACE_TYPE to "virtio"
+fi
+
+if [ -z "$OLVM_CA_CERT_PATH" ]; then
+	echo OLVM_CA_CERT_PATH is not defined
+	exit 1
+fi
+
+if [ -z "$OLVM_VM_TEMPLATE_NAME" ]; then
+	echo OLVM_VM_TEMPLATE_NAME is not defined
+	exit 1
+fi
+
+if [ -z "$OLVM_VM_TEMPLATE_NAME" ]; then
+	echo OLVM_VM_TEMPLATE_NAME is not defined
+	exit 1
+fi
+
+if [ -z "$NAMESERVER_IP" ]; then
+	echo NAMESERVER_IP is not defined
+	exit 1
+fi
+
+
+# Generate the OLVM configuration files
+TEMPLATES=$(find "$TESTDIR" -name clusterConfigTemplate.yaml)
+
+for TEMPLATE in $TEMPLATES; do
+  CONFIG_FILE=${TEMPLATE/%clusterConfigTemplate.yaml}clusterConfig.yaml
+  echo "Generating $CONFIG_FILE"
+  envsubst < "$TEMPLATE" > "$CONFIG_FILE"
+done
---
+++ scenarios/olvm/environment/unsetup.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+git clean -fd
+
+
