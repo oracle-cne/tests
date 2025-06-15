009c75dac07ebb358bc8ff6705abafc0a84d0a6d
Michael Gianatassio <michael.gianatassio@oracle.com>
2025-06-15 07:41:56 -0400

    undo exception changes

--- tests/cleanliness/check-base-layer.sh
+++ tests/cleanliness/check-base-layer.sh
@@ -2,7 +2,7 @@
 set -e
 set -x
 
-EXCEPTION='container-registry.oracle.com/olcne/nginx:1.17.7-1 container-registry.oracle.com/olcne/pause:3.10 container-registry.oracle.com/olcne/pause:3.9 container-registry.oracle.com/olcne/ovirt-csi-driver:v4.20.0 container-registry.oracle.com/olcne/csi-node-driver-registrar:v2.13.0 container-registry.oracle.com/olcne/csi-attacher:v4.8.1 container-registry.oracle.com/olcne/csi-resizer:v1.13.2 container-registry.oracle.com/olcne/csi-provisioner:v5.2.0 container-registry.oracle.com/olcne/livenessprobe:v2.15.0'
+EXCEPTION='container-registry.oracle.com/olcne/nginx:1.17.7-1 container-registry.oracle.com/olcne/pause:3.10 container-registry.oracle.com/olcne/pause:3.9'
 
 BASE_INSPECT="$(podman image inspect container-registry.oracle.com/os/oraclelinux:8)"
 BASE_LAYER=$(echo "$BASE_INSPECT" | yq '.[].RootFS.Layers[0]')
