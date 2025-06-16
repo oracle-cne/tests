2aa4e791e54764f933a5ff5645b00939f36501e4
Michael Gianatassio <michael.gianatassio@oracle.com>
2025-06-16 12:03:50 -0400

    save

--- run_tests.sh
+++ run_tests.sh
@@ -4,7 +4,7 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 TESTDIR=./scenarios/sanity
-TEST_GROUPS=tests/cleanliness tests/functional tests/upgrade
+TEST_GROUPS="tests/cleanliness tests/functional tests/upgrade"
 PATTERN=
 FORMAT=tap
 RESULTS="$(pwd)/$(date +"%Y-%m-%d-%H:%m")"
@@ -60,7 +60,7 @@ for TEST_DIR in $TESTS; do
 	export INFO="$TEST_DIR/info.yaml"
 	export CASE_NAME=$(basename "$TEST_DIR")
 
-	bats --formatter "$FORMAT" --output "$RESULTS"  --setup-suite-file tests/setup/setup --trace --recursive "${TEST_GROUPS}"
+	bats --formatter "$FORMAT" --output "$RESULTS"  --setup-suite-file tests/setup/setup --trace --recursive ${TEST_GROUPS}
 done
 
 ./tools/stop-test-catalog.sh "$USE_PODMAN"
